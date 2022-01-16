4b3dbbdb04a1035bb1921ad382c1af0f92a321a4
Marcos Florentino <marcosnflorentino@gmail.com>
2022-01-16 15:52:27 -0300

    continuando personalização de sidebar com js

--- js/menu.js
+++ js/menu.js
@@ -2,8 +2,9 @@
 let iconMenu = document.getElementById('iconMenu'); //usada para manipular o icone de menu
 let sideBar = document.getElementById('sideBar'); // usada pra manipular a sidebar
 let mainContent = document.getElementById('mainContent'); // usadas para manipular o main 
+let hide = document.getElementById('hide'); // usadas para manipular o hide 
 let w = window.innerWidth; // variavel responsavel para verificar o tamanho da tela
-sideBar.style.left ='-320px';
+sideBar.style.left = '-320px';
 //
 
 //adicionando evento
@@ -11,25 +12,33 @@ window.addEventListener("resize", () => {
     w = window.innerWidth;
     console.log(w);
 })
+if (sideBar <= 768) {
+    sideBar.classList.add(hide);
+} else {
+    sideBar.classList.remove(hide);
+}
 //
 
 //Funcão de menu
 function responsiveSidebar() {
     if (w <= 671) { //condiçaõ para mobile
-        if (sideBar.style.left == '-320px') {
-            sideBar.style.left ='0px';
+        if (sideBar.classList.contains('hide') || sideBar.style.left == '-320px') {
+            sideBar.style.left = '0px';
+            console.log('teste')
+
         } else {
             sideBar.style.left = '-320px';
+            console.log('teste1')
         }
     } else { //condiçao para desktop
         if (sideBar.style.display == 'none') { // condição se estiver ativado o evento
             sideBar.style.display = 'block';
             mainContent.style.width = 'calc(100% - 320px)';
-            mainContent.style.transition  = 'all 0.2s';
+            mainContent.style.transition = 'all 0.3s';
         } else { // condição se não estiver ativado o evento
             sideBar.style.display = 'none';
             mainContent.style.width = '100%';
-            mainContent.style.transition  = 'all 0.2s';
+            mainContent.style.transition = 'all 0.3s';
         }
     }
 }
